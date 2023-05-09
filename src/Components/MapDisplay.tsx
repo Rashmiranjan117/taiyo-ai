798407c561cc6586b53c434d6a15aaf7818b247e
Rashmiranjan117 <107473816+Rashmiranjan117@users.noreply.github.com>
2023-05-09 20:53:18 +0530

    adding basic page structure with basic css using tailwind and redux

---
+++ src/Components/MapDisplay.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MapDisplay = () => {
+  return <div></div>;
+};
+
+export default MapDisplay;
--- src/Pages/Chart.tsx
+++ src/Pages/Chart.tsx
@@ -1,11 +1,71 @@
-import React from 'react'
-
-const Chart = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
+export interface CountryDataType {
+  active: number;
+  activePerOneMillion: number;
+  cases: number;
+  casesPerOneMillion: number;
+  continent: string;
+  country: string;
+  countryInfo: {
+    flag: string;
+    iso2: string;
+    iso3: string;
+    lat: number;
+    long: number;
+    _id: number;
+  };
+  critical: number;
+  criticalPerOneMillion: number;
+  deaths: number;
+  deathsPerOneMillion: number;
+  oneCasePerPeople: number;
+  oneDeathPerPeople: number;
+  oneTestPerPeople: number;
+  population: number;
+  recovered: number;
+  recoveredPerOneMillion: number;
+  tests: number;
+  testsPerOneMillion: number;
+  todayCases: number;
+  todayDeaths: number;
+  todayRecovered: number;
+  updated: number;
 }
 
-export default Chart
+const getData = async (): Promise<CountryDataType[]> => {
+  const { data } = await axios.get<CountryDataType[]>(
+    "https://disease.sh/v3/covid-19/countries"
+  );
+  const flattenedData = data.reduce(
+    (acc: CountryDataType[], curr: CountryDataType) => acc.concat([curr]),
+    []
+  );
+  return flattenedData;
+};
+const Chart = () => {
+  const { data: allData, isLoading } = useQuery<CountryDataType[]>(
+    "countryData",
+    getData
+  );
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  console.log("Data", allData);
+
+  return (
+    <div>
+      {allData &&
+        allData?.map((el, i) => {
+          return (
+            <div key={i}>
+              <h1>{el?.continent}</h1>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+
+export default Chart;
