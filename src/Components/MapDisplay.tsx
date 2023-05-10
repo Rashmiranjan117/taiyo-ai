bccada809c8f27feac9a274939206434cb04db5d
Rashmiranjan117 <107473816+Rashmiranjan117@users.noreply.github.com>
2023-05-10 13:16:31 +0530

    completed first part, bugs on second part

--- src/Components/MapDisplay.tsx
+++ src/Components/MapDisplay.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { LatLngTuple } from "leaflet";
+import axios from "axios";
 
-const MapDisplay = () => {
-  return <div></div>;
+const api = "780394a78657001a17b66be2b1695f61";
+
+interface MapDisplayProps {
+  lat: number;
+  long: number;
+}
+
+
+const MapDisplay = ({ lat, long }: MapDisplayProps) => {
+  const position: LatLngTuple = [lat, long];
+  return (
+    <div style={{ height: "400px", width: "100%" }}>
+      {/* <MapContainer center={[lat,long]} >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={position}>
+          <Popup>Your location</Popup>
+        </Marker>
+      </MapContainer> */}
+    </div>
+  );
 };
 
 export default MapDisplay;
--- src/Redux/ContactReducer/reducer.ts
+++ src/Redux/ContactReducer/reducer.ts
@@ -1,15 +1,19 @@
 import * as types from "./actionTypes";
+import { ContactType } from "../../Components/Create";
 
 interface State {
-  data: any[];
+  data: ContactType[];
   isLoading: boolean;
   isError: boolean;
 }
 
-type Action =
-  | { type: "GET_CONTACT_REQUEST"; payload?: null | any[] }
-  | { type: "GET_CONTACT_SUCCESS"; payload?: any[] }
-  | { type: "GET_CONTACT_ERROR"; payload?: null | any[] };
+// type Action =
+//   | { type: "GET_CONTACT_REQUEST"; payload: [] }
+//   | {
+//       type: "GET_CONTACT_SUCCESS";
+//       payload?: [] ;
+//     }
+//   | { type: "GET_CONTACT_ERROR"; payload: [] };
 
 const initState: State = {
   data: [],
@@ -17,13 +21,22 @@ const initState: State = {
   isError: false,
 };
 
-const reducer = (state: State = initState, action: Action) => {
-  let { type, payload } = action;
+const reducer = (state: State = initState, action: any) => {
+  let { payload, type } = action;
   switch (type) {
     case types.GET_CONTACT_REQUEST:
       return { ...state, isLoading: true, isError: false };
     case types.GET_CONTACT_SUCCESS:
-      return { ...state, isLoading: false, data: payload, isError: false };
+      return {
+        ...state,
+        isLoading: false,
+        data: [...state.data, payload],
+        isError: false,
+      };
+    case types.DELETE_CONTACT:
+      return { ...state, isLoading: false, isError: false, data: payload };
+    case types.EDIT_CONTACT:
+      return { ...state, isLoading: false, isError: false, data: payload };
     case types.GET_CONTACT_ERROR:
       return { ...state, isError: true, isLoading: false };
     default:
